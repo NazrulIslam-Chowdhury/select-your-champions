2d18f8801d2bc563ca6576339048f49670c323e4
rahiislam96 <nazrulislam66188@gmail.com>
2022-08-22 20:38:13 +0600

    infon added

--- js/callOut.function.js
+++ js/callOut.function.js
@@ -1,44 +1,53 @@
-// document.getElementById('button1').addEventListener('click', function () {
-//     selectedPlayers('player1');
-// })
-// document.getElementById('button2').addEventListener('click', function () {
-//     selectedPlayers('player2');
-// })
-// document.getElementById('button3').addEventListener('click', function () {
-//     selectedPlayers('Kylian Mbappe');
-// })
+
 document.getElementById('button1').addEventListener('click', function () {
     players('player1');
-
+    const button1 = document.getElementById('button1');
+    button1.disabled = true;
 })
 
 document.getElementById('button2').addEventListener('click', function () {
     players('player2');
+    const button2 = document.getElementById('button2');
+    button2.disabled = true;
 })
 document.getElementById('button3').addEventListener('click', function () {
     players('player3');
+    const button3 = document.getElementById('button3');
+    button3.disabled = true;
 })
 
 document.getElementById('button4').addEventListener('click', function () {
     players('player4');
+    const button4 = document.getElementById('button4');
+    button4.disabled = true;
 })
 
 document.getElementById('button5').addEventListener('click', function () {
     players('player5');
+    const button5 = document.getElementById('button5');
+    button5.disabled = true;
 })
 
 document.getElementById('button6').addEventListener('click', function () {
     players('player6');
+    const button6 = document.getElementById('button6');
+    button6.disabled = true;
 })
 
 document.getElementById('button7').addEventListener('click', function () {
     players('player7');
+    const button7 = document.getElementById('button7');
+    button7.disabled = true
 })
 
 document.getElementById('button8').addEventListener('click', function () {
     players('player8');
+    const button8 = document.getElementById('button8');
+    button8.disabled = true
 })
 
 document.getElementById('button9').addEventListener('click', function () {
     players('player9');
+    const button9 = document.getElementById('button9');
+    button9.disabled = true
 })
--- js/players.js
+++ js/players.js
@@ -1,43 +1,13 @@
-// function selectPlayers(players) {
-//     const selectedPlayersList = document.getElementById('list-1');
-//     const li1 = document.createElement('li');
-//     li1.innerText = players;
-//     selectedPlayersList.appendChild(li1);
-//     const li2 = document.createElement('li');
-//     li1.innerText = players;
-//     selectedPlayersList.appendChild(li2);
-//     const li3 = document.createElement('li');
-//     li1.innerText = players;
-//     selectedPlayersList.appendChild(li3);
 
-// }
-
-// function selectedPlayers(players) {
-//     const orderedList1 = document.getElementById('list-1');
-//     orderedList1.innerText = players;
-//     return;
-// }
-// function selectedPlayers2(players) {
-//     const orderedList2 = document.getElementById('list-2');
-//     orderedList2.innerText = players;
-//     return
-// }
-// function selectedPlayers3(players) {
-//     const orderedList3 = document.getElementById('list-3');
-//     orderedList3.innerText = players;
-// }
-
-function players(player) {
+function players(player, event) {
+    const nameListContainer = document.getElementById('selected-list');
     const playerNameElement = document.getElementById(player);
     const newPlayerName = playerNameElement.innerText;
 
-    const nameListContainer = document.getElementById('selected-list');
+    const li = document.createElement('li');
 
-    const p = document.createElement('p');
-    p.innerText = newPlayerName;
-    nameListContainer.appendChild(p);
-    if (nameListContainer.childNodes() > 5) {
-        alert('Can not select more than 5 players!');
-        return;
-    }
+    li.innerText = newPlayerName;
+    li.classList.add('item');
+    nameListContainer.appendChild(li);
 }
+
