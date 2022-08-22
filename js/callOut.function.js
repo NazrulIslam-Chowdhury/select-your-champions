c65cc878ea0f5888e6aa644c515a5b55aeabdcb1
rahiislam96 <nazrulislam66188@gmail.com>
2022-08-22 13:43:16 +0600

    calculation section added

--- js/callOut.function.js
+++ js/callOut.function.js
@@ -19,26 +19,26 @@ document.getElementById('button3').addEventListener('click', function () {
     players('player3');
 })
 
-document.getElementById('button3').addEventListener('click', function () {
-    players('player3');
+document.getElementById('button4').addEventListener('click', function () {
+    players('player4');
 })
 
-document.getElementById('button3').addEventListener('click', function () {
-    players('player3');
+document.getElementById('button5').addEventListener('click', function () {
+    players('player5');
 })
 
-document.getElementById('button3').addEventListener('click', function () {
-    players('player3');
+document.getElementById('button6').addEventListener('click', function () {
+    players('player6');
 })
 
-document.getElementById('button3').addEventListener('click', function () {
-    players('player3');
+document.getElementById('button7').addEventListener('click', function () {
+    players('player7');
 })
 
-document.getElementById('button3').addEventListener('click', function () {
-    players('player3');
+document.getElementById('button8').addEventListener('click', function () {
+    players('player8');
 })
 
-document.getElementById('button3').addEventListener('click', function () {
-    players('player3');
+document.getElementById('button9').addEventListener('click', function () {
+    players('player9');
 })
