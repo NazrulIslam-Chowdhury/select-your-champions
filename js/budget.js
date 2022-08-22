01dc8174fe90d7680a68bd89f288da42521d173b
rahiislam96 <nazrulislam66188@gmail.com>
2022-08-22 14:17:33 +0600

    budget.js added

---
+++ js/budget.js
@@ -0,0 +1,29 @@
+function recruitmentCost(fieldId) {
+    const recruitmentPriceField = document.getElementById(fieldId);
+    const recruitmentPriceString = recruitmentPriceField.value;
+    const recruitmentPrice = parseInt(recruitmentPriceString);
+    return recruitmentPrice;
+}
+
+function setTextElementById(elementId, newValue) {
+    const totalPlayerPriceElement = document.getElementById(elementId);
+    totalPlayerPriceElement.innerText = newValue;
+}
+
+document.getElementById('calculation').addEventListener('click', function () {
+    const recruitmentPlayerPrice = recruitmentCost('perP-cost');
+
+    const totalPlayerPrice = recruitmentPlayerPrice * 5;
+    setTextElementById('player-expenses', totalPlayerPrice);
+})
+
+document.getElementById('total-calculation').addEventListener('click', function () {
+    const recruitmentManagerPrice = recruitmentCost('manager-cost');
+    const recruitmentCoahPrice = recruitmentCost('coach-cost');
+
+    const recruitmentPlayerPrice = recruitmentCost('perP-cost');
+    const totalPlayerPrice = recruitmentPlayerPrice * 5;
+
+    const finalRecruitmentCost = totalPlayerPrice + recruitmentManagerPrice + recruitmentCoahPrice;
+    setTextElementById('calculate-total', finalRecruitmentCost);
+})
